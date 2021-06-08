961f3b2a507f77cb00ac37e3d8d348e07c0a21d1
Jank-MG <68519584+Jank-MG@users.noreply.github.com>
2021-06-08 11:07:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+function myFunction() {
+  // get the clock
+  var myClock = document.getElementById("menu");
+
+  // get the current value of the clock's display property
+  var displaySetting = myClock.style.display;
+
+  // also get the clock button, so we can change what it says
+  var clockButton = document.getElementById("menuButton");
+
+  // now toggle the clock and the button text, depending on current state
+  if (displaySetting == "block") {
+    // clock is visible. hide it
+    myClock.style.display = "none";
+    // change button text
+  } else {
+    // clock is hidden. show it
+    myClock.style.display = "block";
+    // change button text
+  }
+}
+
+function myFunctionNov1() {
+  // get the clock
+  var myClock = document.getElementById("nov1-text");
+
+  // get the current value of the clock's display property
+  var displaySetting = myClock.style.display;
+
+  // also get the clock button, so we can change what it says
+  var clockButton = document.getElementById("menuButton");
+
+  // now toggle the clock and the button text, depending on current state
+  if (displaySetting == "block") {
+    // clock is visible. hide it
+    myClock.style.display = "none";
+    // change button text
+  } else {
+    // clock is hidden. show it
+    myClock.style.display = "block";
+    // change button text
+  }
+}
+
+function myFunctionNov2() {
+  // get the clock
+  var myClock = document.getElementById("nov2-text");
+
+  // get the current value of the clock's display property
+  var displaySetting = myClock.style.display;
+
+  // also get the clock button, so we can change what it says
+  var clockButton = document.getElementById("menuButton");
+
+  // now toggle the clock and the button text, depending on current state
+  if (displaySetting == "block") {
+    // clock is visible. hide it
+    myClock.style.display = "none";
+    // change button text
+  } else {
+    // clock is hidden. show it
+    myClock.style.display = "block";
+    // change button text
+  }
+}
+
+function myFunctionNov3() {
+  // get the clock
+  var myClock = document.getElementById("nov3-text");
+
+  // get the current value of the clock's display property
+  var displaySetting = myClock.style.display;
+
+  // also get the clock button, so we can change what it says
+  var clockButton = document.getElementById("menuButton");
+
+  // now toggle the clock and the button text, depending on current state
+  if (displaySetting == "block") {
+    // clock is visible. hide it
+    myClock.style.display = "none";
+    // change button text
+  } else {
+    // clock is hidden. show it
+    myClock.style.display = "block";
+    // change button text
+  }
+}
